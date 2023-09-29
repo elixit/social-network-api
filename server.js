b90ced16fbefe6f9d0c6d2798200125842e04ea5
Ismail <63372291+elixit@users.noreply.github.com>
2023-09-28 19:29:03 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
 const express = require("express");
 const db = require("./config/connection");
 const routes = require("./routes");
-const mongoose = require('mongoose')
-// required modules addded
 
-// connecting to port
-const PORT = process.env.PORT || 3001;
 const app = express();
-
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-
-
+app.use(routes);
 
 db.once("open", () => {
-  app.listen(PORT, () => console.log(`on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
   });
+});
