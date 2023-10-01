4cbc801af917de6927f6db5bb585bb83381a13c0
Ismail <63372291+elixit@users.noreply.github.com>
2023-10-01 03:52:58 -0700

    Delete routes directory

--- routes/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const userRoutes = require("./userRoutes");
-const thoughtRoutes = require("./thoughtRoutes");
-// using routers and accessing from the correct folder
-
-router.use("/users", userRoutes);
-router.use("/thoughts", thoughtRoutes);
-
-module.exports = router;
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-// adding required api routes
-const router = require("express").Router();
-const apiRoutes = require("./api");
-
-router.use("/api", apiRoutes);
-
-router.use((req, res) => res.send('change route'));
-
-module.exports = router;
